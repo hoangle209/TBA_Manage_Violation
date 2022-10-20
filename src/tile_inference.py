db4da0b56b2d80e63fc9f66ffe7539cee5dbe526
hoangle209 <112451598+hoangle209@users.noreply.github.com>
2022-10-20 09:47:01 +0700

    + tile inference video check isolation

--- src/tile_inference.py
+++ src/tile_inference.py
@@ -46,6 +46,7 @@ def tile_inference(img_path, model, size = (1152, 1152), patch_size = 576, conf_
 
                 textw, texth = cv.getTextSize(f'{label_dict[int(bb[-1])]} {bb[-2]:0.2f}', 
                                               cv.FONT_HERSHEY_SIMPLEX, 0.5, 2)[0]
+                
                 cv.rectangle(_img, (xmin, max(ymin - texth - 2, 0)), (xmin + textw, ymin), color_dict[int(bb[-1])], -1)
 
                 cv.putText(_img, f'{label_dict[int(bb[-1])]} {bb[-2]:0.2f}', 
@@ -59,33 +60,78 @@ def tile_inference(img_path, model, size = (1152, 1152), patch_size = 576, conf_
         return True
     return _img     
    
-   
-def tile_inference_boxes(img_path, model, size = (1152, 1152), patch_size = 576, conf_thresh = 0.15, rtype = 'xyxy'):
-    img = Image.open(img_path).convert('RGB').resize(size)
-    _img = np.array(img)
-    num_patches_w = int(size[0] / patch_size)
-    num_patches_h = int(size[1] / patch_size)
 
-    bboxes = []
-    for k in range(num_patches_w):
-        for h in range(num_patches_h):
-            xmin, ymin = k*patch_size, h*patch_size
-            xmax, ymax = (k+1)*patch_size, (h+1)*patch_size
+def set_config(size = (1728, 1728), patch_size = 576, conf_thresh = 0.15):
+    size = size
+    patch_size = patch_size
+    conf_thresh = conf_thresh
+    return size, patch_size, conf_thresh
+    
+def tile_inference_video_check_isolation(cam_id):
+    color_dict = {
+        0: (0, 138, 0),
+        1: (255, 0, 0),
+        2: (255, 255, 0),
+        3: (255, 255, 255),
+        4: (0, 0, 255),
+        5: (255, 165, 0), 
+        6: (255, 0, 255)
+    }
+    label_dict = {
+        0: 'None',
+        1: 'Red',
+        2: 'Yellow',
+        3: 'White',
+        4: 'Blue',
+        5: 'Orange', 
+        6: 'Others'
+    }
 
-            img_i = img.crop((int(xmin), int(ymin), int(xmax), int(ymax)))
-            r = model(img_i)
-            xyxy = r.xyxyn[0].cpu().numpy()
-            
-            for bb in xyxy:
-                if bb[-2] < conf_thresh: 
-                    continue
+    restricted_area = [] # TODO
+    labels = [-1] # TODO
 
-                xmin = int((bb[0] + k) * patch_size)
-                ymin = int((bb[1] + h) * patch_size)
-                xmax = int((bb[2] + k) * patch_size)
-                ymax = int((bb[3] + h) * patch_size)
-                if rtype == 'xyxy':
-                    bboxes.append([xmin, ymin, xmax, ymax, bb[-1]])
-                elif rtype == 'xywh':
-                    bboxes.append([(xmin + xmax)/2, (ymin + ymax)/2, xmax - xmin, ymax - ymin])
-    return bboxes          
+    size, patch_size, conf_thresh = set_config()
+    vid = cv.VideoCapture(cam_id)
+    while True:
+        ret, cap = vid.read()
+        _img = cap.copy()
+        _img = _img[..., ::-1]
+        if ret:
+            _img = cv.resize(_img, size)
+            num_patches_w = int(size[0] / patch_size)
+            num_patches_h = int(size[1] / patch_size)
+            for k in range(num_patches_w):
+                for h in range(num_patches_h):
+                    xmin, ymin = k*patch_size, h*patch_size
+                    xmax, ymax = (k+1)*patch_size, (h+1)*patch_size
+
+                    img_i = _img[ymin:ymax, xmin:xmax]
+                    r = model(img_i)
+                    xyxy = r.xyxyn[0].cpu().numpy()
+                    
+                    for bb in xyxy:
+                        if bb[-2] < conf_thresh: 
+                            continue
+
+                        xmin = int((bb[0] + k) * patch_size)
+                        ymin = int((bb[1] + h) * patch_size)
+                        xmax = int((bb[2] + k) * patch_size)
+                        ymax = int((bb[3] + h) * patch_size)
+
+                        cv.rectangle(_img, (xmin, ymin), (xmax, ymax), color_dict[int(bb[-1])], 2)
+
+                        textw, texth = cv.getTextSize(f'{label_dict[int(bb[-1])]} {bb[-2]:0.2f}', 
+                                                      cv.FONT_HERSHEY_SIMPLEX, 0.5, 2)[0]
+                        cv.rectangle(_img, (xmin, max(ymin - texth - 2, 0)), (xmin + textw, ymin), color_dict[int(bb[-1])], -1)
+
+                        cv.putText(_img, f'{label_dict[int(bb[-1])]} {bb[-2]:0.2f}', 
+                                  (xmin, max(ymin-2, 0)), 
+                                  cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+                        
+                        cx, cy = (xmin+xmax)/2, (ymin+ymax)/2
+                        violate = _check_violate([cx, cy, bb[-1]], restricted_area, labels)
+                        print('Warning') # TODO 
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv.release()
+    cv.destroyAllWindows()
